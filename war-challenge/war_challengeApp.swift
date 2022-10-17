56d52fe0035c64cf627cca3e6d8f979c52282a82
YouAteMyPie <114132528+YouAteMyPie@users.noreply.github.com>
2022-10-16 17:48:06 -0700

    Add files via upload

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Joseph Lee on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
